e736ce24fe1c783e72cbfc53a5b4ef936e074da7
evodata123 <evodata.info@gmail.com>
2024-06-01 20:20:35 +0530

    product page

---
+++ src/app/products/page.tsx
@@ -0,0 +1,27 @@
+import AboutSectionOne from "@/components/About/AboutSectionOne";
+import AboutSectionTwo from "@/components/About/AboutSectionTwo";
+import Breadcrumb from "@/components/Common/Breadcrumb";
+
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Products Page |  Evanta Analytics and Consulting",
+  description: "This page lists the customisable and AI enabled products built by Evanta Analytics and Consulting. #Customer Relationship Management #Intelligent Systems",
+  // other metadata
+};
+
+const ProductsPage = () => {
+  return (
+    <>
+    <br></br>
+      <Breadcrumb
+        pageName="Products"
+        description="Evanta Analytics & Consulting believes in delivering user friendly intuitive products. Our range of products consists of various such amazing software."
+      />
+      <AboutSectionOne />
+      <AboutSectionTwo />
+    </>
+  );
+};
+
+export default ProductsPage;
